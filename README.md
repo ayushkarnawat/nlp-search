035ab8e6d8ad4562de6f71d2f3fdf12860431cdb
Daniel Yakobian <daniel.yakobian@gotinder.com>
2017-07-25 21:23:04 -0700

    Make updates for ayush

---
+++ README.md
@@ -0,0 +1,14 @@
+# Flightfutures NLP 
+
+#### Setup 
+- `pip install -r requirement.txt`
+- `python` and then 
+```
+import nltk
+nltk.download('all') # nltk.download_shell('all') on MAC
+``` 
+
+#### Testing
+Individual: `python -m unittest nlp.test.search_test`
+
+All: `python -m unittest discovery`
--- nlp/test/search/test_search.py
+++ nlp/test/search/test_search.py
@@ -3,7 +3,7 @@ import unittest
 import nltk
 from nltk.tokenize import word_tokenize
 
-from nlp_search import filter, get_origin_and_destination, get_dates
+from nlp.search import search
 
 
 class TestNLPSearch(unittest.TestCase):
@@ -11,13 +11,13 @@ class TestNLPSearch(unittest.TestCase):
     def test_filter(self):
         # Test case #1: Untagged words
         raw = "Flights from New York to Los Angeles between October 2nd to November 21st"
-        filtered_sentence_without_tags = filter(raw, get_tags=False, language="english")
+        filtered_sentence_without_tags = search.filter(raw, get_tags=False, language="english")
         result = ['Flights', 'New', 'York', 'Los', 'Angeles', 'October', '2nd', 'November', '21st']
         self.assertEquals(filtered_sentence_without_tags, result)
 
         # Test case #2: Proper word string tagging based on parts of speech
         raw = "Flights from New York to Los Angeles between October 2nd to November 21st"
-        filtered_sentence_with_tags = filter(raw, get_tags=True, language="english")
+        filtered_sentence_with_tags = search.filter(raw, get_tags=True, language="english")
         result_tagged = [('Flights', 'NNS'), ('New', 'NNP'), ('York', 'NNP'), ('Los', 'NNP'), ('Angeles', 'NNP'), ('October', 'NNP'),                      ('2nd', 'CD'), ('November', 'NNP'), ('21st', 'CD')]
         self.assertEquals(filtered_sentence_with_tags, result_tagged)
 
@@ -27,7 +27,7 @@ class TestNLPSearch(unittest.TestCase):
         raw = "Flights from JFK to LAX between October 2nd to November 21st"
         words = word_tokenize(raw)
         tagged_words = nltk.pos_tag(words)
-        parsed_tree = get_origin_and_destination(tagged_words)
+        parsed_tree = search.get_origin_and_destination(tagged_words)
         output_airport_code = "(S\n  Flights/NNS\n  from/IN\n  (ORGIN/DESTINATION JFK/NNP to/TO LAX/NNP)\n  between/IN\n  October/NNP\n  2nd/CD\n  to/TO\n  November/NNP\n  21st/CD)"
         self.assertEquals(parsed_tree.__str__(), output_airport_code)
 
@@ -35,7 +35,7 @@ class TestNLPSearch(unittest.TestCase):
         raw = "Flights from New York to Los Angeles between October 2nd to November 21st"
         words = word_tokenize(raw)
         tagged_words = nltk.pos_tag(words)
-        parsed_tree = get_origin_and_destination(tagged_words)
+        parsed_tree = search.get_origin_and_destination(tagged_words)
         output_city_names = "(S\n  Flights/NNS\n  from/IN\n  (ORGIN/DESTINATION New/NNP York/NNP to/TO Los/NNP Angeles/NNP)\n  between/IN\n  October/NNP\n  2nd/CD\n  to/TO\n  November/NNP\n  21st/CD)"
         self.assertEquals(parsed_tree.__str__(), output_city_names)
 
@@ -44,17 +44,17 @@ class TestNLPSearch(unittest.TestCase):
         raw = "Flights from New York to LAX between October 2nd to November 21st"
         words = word_tokenize(raw)
         tagged_words = nltk.pos_tag(words)
-        parsed_tree = get_origin_and_destination(tagged_words)
+        parsed_tree = search.get_origin_and_destination(tagged_words)
         output_city_code = "(S\n  Flights/NNS\n  from/IN\n  (ORGIN/DESTINATION New/NNP York/NNP to/TO LAX/NNP)\n  between/IN\n  October/NNP\n  2nd/CD\n  to/TO\n  November/NNP\n  21st/CD)"
         self.assertEquals(parsed_tree.__str__(), output_city_code)
 
-    
+
     def test_get_dates(self):
         # Test case #1: Both departure and return date are provided
         raw = "Flights from New York to LAX from October 2nd to November 21st"
         words = word_tokenize(raw)
         tagged_words = nltk.pos_tag(words)
-        parsed_tree = get_dates(tagged_words)
+        parsed_tree = search.get_dates(tagged_words)
         output_departure_return_dates = "(S\n  Flights/NNS\n  from/IN\n  New/NNP\n  York/NNP\n  to/TO\n  LAX/VB\n  from/IN\n  (Departure/Return October/NNP 2nd/CD to/TO November/NNP 21st/CD))"
         self.assertEquals(parsed_tree.__str__(), output_departure_return_dates)
 
@@ -62,12 +62,10 @@ class TestNLPSearch(unittest.TestCase):
         raw = "Flights from New York to LAX on October 2nd"
         words = word_tokenize(raw)
         tagged_words = nltk.pos_tag(words)
-        parsed_tree = get_dates(tagged_words)
+        parsed_tree = search.get_dates(tagged_words)
         output_departure_date = "(S\n  Flights/NNS\n  from/IN\n  New/NNP\n  York/NNP\n  to/TO\n  LAX/VB\n  on/IN\n  (Departure/Return October/NNP 2nd/CD))"
         self.assertEquals(parsed_tree.__str__(), output_departure_date)
 
-if __name__ == "__main__":
-    search = TestNLPSearch()
-    filtered_sentences = search.test_filter()
-    origin_detination = search.test_get_origin_and_destination()
-    dates = search.test_get_dates()
+
+if __name__ == '__main__':
+    unittest.main()
